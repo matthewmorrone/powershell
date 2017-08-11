46669791a7e6e67bd81aff8ce017c00be7d54121
Matthew Morrone <matthewmorrone1@gmail.com>
2017-08-11 15:02:17 -0400

    fancier image processing

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,32 +1,206 @@
 $Shell = $Host.UI.RawUI
-$Shell.ForegroundColor       = "White"
-$Shell.BackgroundColor       = "Black"
-$Shell.CursorSize            = 100
+$Shell.ForegroundColor			= "White"
+$Shell.BackgroundColor			= "Black"
+$Shell.CursorSize				= 100
 
-# $Shell.BufferSize            = (200,5000)
+# $Shell.BufferSize				= (200,5000)
 $value = $Shell.BufferSize
 $value.Width = 200
 $value.Height = 5000
 $Shell.BufferSize = $value
 
-# $Shell.WindowSize            = (200,50)
+# $Shell.WindowSize				= (200,50)
 $value = $Shell.WindowSize
 $value.Width = 200
 $value.Height = 50
 $Shell.WindowSize = $value
 
 
-Set-Location C:\Users\Matthew\downloads
-$MaximumHistoryCount = 32KB-1
+Set-Location C:\Users\Matthew\Downloads
+$MaximumHistoryCount = 16KB-1
 
 Set-Alias -Name wget -Value 'C:\wget\wget.exe' -Option AllScope
 Set-Alias subl 'C:\Program Files\Sublime Text 3\sublime_text.exe'
 Set-Alias np 'C:\Windows\System32\notepad.exe'
-Set-Alias brc 'BRC64'
+Set-Alias py 'python'
+Set-Alias brc 'brc64'
+
+Set-Alias rmsvg 'rm *.svg'
+
+# pushd popd
+
+
+function htdocs 	{ cd C:\Users\Matthew\Desktop\htdocs }
+function desktop 	{ cd C:\Users\Matthew\Desktop }
+function documents 	{ cd C:\Users\Matthew\Documents }
+function downloads 	{ cd C:\Users\Matthew\Downloads }
+
+function sshm 		{ ssh matthewmorrone@matthewmorrone.com }
+
+function svg 	{
+	mogrify -size x512 -background none -format png *.svg
+	# rm *.svg
+}
+Set-Alias rename 'Rename-Vectorized-Icons'
+Set-Alias mog 'svg'
+
+# function SVG-to-PNG {
+# 	param([Parameter(ValueFromPipeline = $true)]$path)
+# 	process {
+# 		if(!($path)){
+# 			$path=$pwd
+# 		}
+# 		$files = Get-ChildItem $path -Filter *.svg	| select FullName # -Recurse
+# 		$count = $files.Count
+# 		$i = 0
+# 		$outfile = ""
+# 		$origpos = $host.UI.RawUI.CursorPosition
+# 		while ($i -lt $count + 1) {
+# 			$outfile = $files[$i].FullName
+# 			magick convert $outfile -size x512 -background none -format png $outfile
+# 			$i++
+# 			Write-Output "$outfile`t`t$i / $count							"
+# 			$perc = ($i / $count) * 100
+# 			# Write-Progress -Activity "$outfile" -PercentComplete $perc
+# 			# write-output $i
+# 			$host.UI.RawUI.CursorPosition = $origpos
+# 		}
+# 		# $host.UI.RawUI.CursorPosition = $origpos
+# 		# Write-Host 'Complete'
+# 	}
+# }
 
 
 
-function Sublime
+
+
+
+function png 	{mogrify -format png -identify *.svg,*.jpg,*.gif,*.webp}
+function trim 	{mogrify -trim +repage -identify *.png}
+function limit 	{mogrify -resize 512x512> -identify *.png}
+function pad 	{mogrify -background transparent -gravity center -extent 512x512 -identify *.png}
+
+
+function Split-Vectors {
+	param([Parameter(ValueFromPipeline = $true)]$path)
+	process {
+		if(!($path)){
+			$path=$pwd
+		}
+		php ${HOME}/desktop/htdocs/php/split.php $path
+	}
+}
+
+
+function Process-Icons {
+	param([Parameter(ValueFromPipeline = $true)]$path)
+	process {
+		if(!($path)){
+			$path=$pwd
+		}
+		php ${HOME}/desktop/htdocs/php/icon.php $path
+	}
+}
+function Rename-Vectorized-Icons {
+	param([Parameter(ValueFromPipeline = $true)]$path)
+	process {
+		if(!($path)){
+			$path=$pwd
+		}
+		php ${HOME}/desktop/htdocs/php/rename.php $path
+	}
+}
+
+function Resize-Icons {
+	param([Parameter(ValueFromPipeline = $true)]$size)
+	process {
+		if(!($size)){
+			$size=256
+		}
+		$dimensions = "{0}x{0}" -f $size
+		mogrify -resize $dimensions -identify $path/*.png
+	}
+}
+
+function Fix-Icons {
+	param([Parameter(ValueFromPipeline = $true)]$path)
+	begin{
+		[System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") |Out-Null
+	}
+	process {
+		if(!($path)){
+			$path=$pwd
+		}
+		$i = 0
+		$files = Get-ChildItem $path -Filter *.png | % {
+			$image = [System.Drawing.Image]::FromFile($_.FullName)
+			New-Object PSObject -Property @{
+				Index = $i
+				Name = $_.Name
+				BaseName = $_.BaseName
+				Length = $_.Length
+				Directory = $_.Directory
+				Exists = $_.Exists
+				FullName = $_.FullName
+				Extension = $_.Extension
+				Width  = $image.Width
+				Height = $image.Height
+			}
+			$i++
+			$image.Dispose()
+		}
+
+		$count = $files.Count
+		$index = $count - 1
+		while ($index -gt -1) {
+			$image = $files[$index]
+			$w1 = $image.Width
+			$h1 = $image.Height
+			$outfile = $image.FullName
+
+			magick convert $outfile -trim $outfile
+			magick convert $outfile -set option:size '%[fx:max(w,h)]x%[fx:max(w,h)]' xc:none +swap -gravity center -background none -composite $outfile
+
+			$image = [System.Drawing.Image]::FromFile($outfile)
+			$w2 = $image.Width
+			$h2 = $image.Height
+			$image.Dispose()
+
+			if ($w1 -ne $w2 -or $h1 -ne $h2) {
+				Write-Output "$outfile $w1,$h1 $w2,$h2"
+			}
+			$index--
+		}
+
+	}
+}
+
+
+function Fix-Icon-Dimensions {
+	param([Parameter(ValueFromPipeline = $true)]$path)
+	process {
+		if(!($path)){
+			$path=$pwd
+		}
+		$files = Get-ChildItem $path -Filter *.png	| select FullName # -Recurse
+		$count = $files.Count
+		$i = $count - 1
+		$outfile = ""
+		while ($i -gt 0) {
+			$outfile = $files[$i].FullName
+			# magick convert $outfile -set option:size '%[fx:max(w,h)]x%[fx:max(w,h)]' xc:none +swap -gravity center -composite $outfile
+			# magick convert $outfile -set option:size '%[fx:max(w,h)]x%[fx:max(w,h)]' xc:none +swap -gravity center -extent 512x512 -background none -composite $outfile
+			magick convert $outfile -set option:size '%[fx:max(w,h)]x%[fx:max(w,h)]' xc:none +swap -gravity center -background none -composite $outfile
+			if ($i % 10 -eq 0) {
+				Write-Output "$i $outfile"
+			}
+			$i--
+		}
+	}
+}
+
+
+function sublime
 {
 	param([Parameter(ValueFromPipeline = $true)]$path)
 	process
@@ -39,11 +213,9 @@ function Sublime
 	}
 }
 
-function RemoveEmptyDirectories
-{
+function RemoveEmptyDirectories {
 	param([Parameter(ValueFromPipeline = $true)]$path)
-	process
-	{
+	process {
 		if(!($path)){
 			$path=$pwd
 		}
@@ -54,28 +226,22 @@ function RemoveEmptyDirectories
 	}
 }
 
-function FixDimensions
-{
-	param([Parameter(ValueFromPipeline = $true)]$path)
-	process
-	{
-		if(!($path)){
-			$path=$pwd
-		}
-		$files = Get-ChildItem $path -Filter *.png
-		$count = $files.Count
-		$i = $count - 1
-		$outfile = ""
-		while ($i -gt 0) {
-			$outfile = $files[$i].Name
-			convert $outfile -set option:size '%[fx:max(w,h)]x%[fx:max(w,h)]' xc:none +swap -gravity center -composite $outfile
-			if ($i % 10 -eq 0) {
-				Write-Output "$i	$outfile"
-			}
-			$i--
+
+function Reload-Profile {
+	@(
+		$Profile.AllUsersAllHosts,
+		$Profile.AllUsersCurrentHost,
+		$Profile.CurrentUserAllHosts,
+		$Profile.CurrentUserCurrentHost
+	) | % {
+		if(Test-Path $_){
+		Write-Verbose "Running $_"
+		. $_
 		}
 	}
 }
+
+
 
 function PortView
 {
@@ -83,15 +249,15 @@ function PortView
 	Get-Process | ForEach-Object { $proc.Add($_.Id, $_) };
 	netstat -aon | Select-String "\s*([^\s]+)\s+([^\s]+):([^\s]+)\s+([^\s]+):([^\s]+)\s+([^\s]+)?\s+([^\s]+)" | ForEach-Object {
 		$g = $_.Matches[0].Groups;
-		New-Object PSObject | 
-			Add-Member @{ Protocol =           $g[1].Value  } -PassThru |
-			Add-Member @{ LocalAddress =       $g[2].Value  } -PassThru |
-			Add-Member @{ LocalPort =     [int]$g[3].Value  } -PassThru |
-			Add-Member @{ RemoteAddress =      $g[4].Value  } -PassThru |
-			Add-Member @{ RemotePort =         $g[5].Value  } -PassThru |
-			Add-Member @{ State =              $g[6].Value  } -PassThru |
-			Add-Member @{ PID =           [int]$g[7].Value  } -PassThru |
-			Add-Member @{ Process = $proc[[int]$g[7].Value] } -PassThru;
+		New-Object PSObject |
+			Add-Member @{ Protocol =				$g[1].Value	} -PassThru |
+			Add-Member @{ LocalAddress =			$g[2].Value	} -PassThru |
+			Add-Member @{ LocalPort =				[int]$g[3].Value	} -PassThru |
+			Add-Member @{ RemoteAddress =			$g[4].Value	} -PassThru |
+			Add-Member @{ RemotePort =				$g[5].Value	} -PassThru |
+			Add-Member @{ State =					$g[6].Value	} -PassThru |
+			Add-Member @{ PID =					[int]$g[7].Value	} -PassThru |
+			Add-Member @{ Process = 	$proc[[int]$g[7].Value] } -PassThru ;
 	#
 	} | Format-Table Protocol,LocalAddress,LocalPort,RemoteAddress,RemotePort,State -GroupBy @{Name='Process';Expression={$p=$_.Process;@{$True=$p.ProcessName; $False=$p.MainModule.FileName}[$p.MainModule -eq $Null] + ' PID: ' + $p.Id}} -AutoSize
 }
@@ -106,30 +272,6 @@ function Encrypt
 		ConvertFrom-SecureString
 	}
 }
-
-
-function Play-Sound
-{
-	# find first available WAV file in Windows
-	$WAVPath = Get-ChildItem -Path $env:windir -Filter *.wav -Recurse -ErrorAction SilentlyContinue |
-	Select-Object -First 1 -ExpandProperty FullName
-	# load file and play it
-	$player = New-Object Media.SoundPlayer $WAVPath
-	try
-	{
-		$player.PlayLooping()
-		'Doing something...'
-		1..100 | ForEach-Object {
-			Write-Progress -Activity 'Doing Something. Hang in' -Status $_ -PercentComplete $_
-			Start-Sleep -MilliSeconds (Get-Random -Minimum 300 -Maximum 1300)
-		}
-	}
-	finally
-	{
-		$player.Stop()
-	}
-}
-
 function Decrypt
 {
 	param([Parameter(ValueFromPipeline = $true)]$Text)
@@ -181,7 +323,6 @@ function Get-IP
 	[System.Net.Dns]::GetHostAddresses($ComputerName).IPAddressToString
 }
 
-
 function Get-IPv4
 {
 	$ComputerName = ''
@@ -214,84 +355,37 @@ function Get-Raw
 }
 
 # -----------------------------------------------------------------------------
-# Script: Get-FileMetaDataReturnObject.ps1
-# Author: ed wilson, msft
-# Date: 01/24/2014 12:30:18
-# Keywords: Metadata, Storage, Files
-# comments: Uses the Shell.APplication object to get file metadata
-# Gets all the metadata and returns a custom PSObject
-# it is a bit slow right now, because I need to check all 266 fields
-# for each file, and then create a custom object and emit it.
-# If used, use a variable to store the returned objects before attempting
-# to do any sorting, filtering, and formatting of the output.
-# To do a recursive lookup of all metadata on all files, use this type
-# of syntax to call the function:
+# Gets all the metadata and returns a custom PSObject it is a bit slow right now, because I need to check all 266 fields for each file, and then create a custom object and emit it.
+# If used, use a variable to store the returned objects before attempting to do any sorting, filtering, and formatting of the output.
+# To do a recursive lookup of all metadata on all files, use this type of syntax to call the function:
 # Get-FileMetaData -folder (gci e:\music -Recurse -Directory).FullName
 # note: this MUST point to a folder, and not to a file.
 # -----------------------------------------------------------------------------
 Function Get-FileMetaData
 {
-  <#
-   .Synopsis
-	This function gets file metadata and returns it as a custom PS Object 
-   .Description
-	This function gets file metadata using the Shell.Application object and
-	returns a custom PSObject object that can be sorted, filtered or otherwise
-	manipulated.
-   .Example
-	Get-FileMetaData -folder "e:\music"
-	Gets file metadata for all files in the e:\music directory
-   .Example
-	Get-FileMetaData -folder (gci e:\music -Recurse -Directory).FullName
-	This example uses the Get-ChildItem cmdlet to do a recursive lookup of 
-	all directories in the e:\music folder and then it goes through and gets
-	all of the file metada for all the files in the directories and in the 
-	subdirectories.  
-   .Example
-	Get-FileMetaData -folder "c:\fso","E:\music\Big Boi"
-	Gets file metadata from files in both the c:\fso directory and the
-	e:\music\big boi directory.
-   .Example
-	$meta = Get-FileMetaData -folder "E:\music"
-	This example gets file metadata from all files in the root of the
-	e:\music directory and stores the returned custom objects in a $meta 
-	variable for later processing and manipulation.
-   .Parameter Folder
-	The folder that is parsed for files 
-   .Notes
-	NAME:  Get-FileMetaData
-	AUTHOR: ed wilson, msft
-	LASTEDIT: 01/24/2014 14:08:24
-	KEYWORDS: Storage, Files, Metadata
-	HSG: HSG-2-5-14
-   .Link
-	 Http://www.ScriptingGuys.com
- #Requires -Version 2.0
- #>
- Param([string[]]$folder)
- foreach($sFolder in $folder)
-  {
-   $a = 0
-   $objShell = New-Object -ComObject Shell.Application
-   $objFolder = $objShell.namespace($sFolder)
+	Param([string[]]$folder)
+	foreach($sFolder in $folder)
+	{
+		$a = 0
+		$objShell = New-Object -ComObject Shell.Application
+		$objFolder = $objShell.namespace($sFolder)
 
-   foreach ($File in $objFolder.items())
-	{ 
-	 $FileMetaData = New-Object PSOBJECT
-	  for ($a ; $a  -le 266; $a++)
-	   { 
-		 if($objFolder.getDetailsOf($File, $a))
-		   {
-			 $hash += @{$($objFolder.getDetailsOf($objFolder.items, $a))  =
-				   $($objFolder.getDetailsOf($File, $a)) }
-			$FileMetaData | Add-Member $hash
-			$hash.clear() 
-		   } #end if
-	   } #end for 
-	 $a=0
-	 $FileMetaData
-	} #end foreach $file
-  } #end foreach $sfolder
+		foreach ($File in $objFolder.items())
+		{
+			$FileMetaData = New-Object PSOBJECT
+			for ($a ; $a	-le 266; $a++)
+			{
+				if($objFolder.getDetailsOf($File, $a))
+				{
+						$hash += @{$($objFolder.getDetailsOf($objFolder.items, $a))	= $($objFolder.getDetailsOf($File, $a)) }
+						$FileMetaData | Add-Member $hash
+						$hash.clear()
+				} #end if
+			} #end for
+			$a=0
+			$FileMetaData
+		} #end foreach $file
+	} #end foreach $sfolder
 } #end Get-FileMetaData
 
 
